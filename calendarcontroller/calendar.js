3658aa43ef74de9460c9287b554b5e21b7196ae8
Rishan.Shah <Rishan.Shah@tuwig10shanshah.home>
2019-06-15 15:18:18 +0100

    changed time errors and added descriptions

--- calendarcontroller/calendar.js
+++ calendarcontroller/calendar.js
@@ -30,20 +30,25 @@ exports.createIcs = function(categories){
         // console.log('-----------');
         // console.log($(this).find('.ai1ec-date a').attr('href').split('/')[5].replace('exact_date~','').split('-'));
         // console.log($(this).find('.ai1ec-event-title').text().trim());
-        // console.log($(this).find('.ai1ec-event-time').text().split('@')[1]);
+        //console.log($(this).find('.ai1ec-event-time').text().split('@')[1]);
+        //console.log($(this).find('.ai1ec-popover .ai1ec-popup-excerpt').text());
         var dateArr = $(this).find('.ai1ec-date a').attr('href').split('/')[5].replace('exact_date~','').split('-')
-        var timeOfEvent = $(this).find('.ai1ec-event-time').text().split('@')[1]
+        var timeOfEvent = $(this).find('.ai1ec-popover .ai1ec-event-time').text().split('@')[1]
+        var descriptionstr = $(this).find('.ai1ec-popover .ai1ec-popup-excerpt').text()
+
 
         if(timeOfEvent != undefined){
           timeOfEvent = timeOfEvent.trim().split(' ')
+          //console.log(timeOfEvent[4].substring(0,2))
           var startTime = timeOfEvent[0]+ timeOfEvent[1]
-          var endTime =  timeOfEvent[3]+ timeOfEvent[4]
+          var endTime =  timeOfEvent[3]+ timeOfEvent[4].substring(0,2)
           //console.log(amPm(startTime)[0],amPm(endTime));
 
           eventsArr.push({
             title: $(this).find('.ai1ec-event-title').text().trim(),
             start: [dateArr[2],dateArr[1],dateArr[0],amPm(startTime)[0],amPm(startTime)[1]],
-            end: [dateArr[2],dateArr[1],dateArr[0],amPm(endTime)[0],amPm(endTime)[1]]
+            end: [dateArr[2],dateArr[1],dateArr[0],amPm(endTime)[0],amPm(endTime)[1]],
+            description:descriptionstr
             })
 
         }
